2d8b7311d5973ba0dd21cb01f27f59043e568c89
hwa <hwangsungmin_00@naver.com>
2022-02-11 06:30:27 +0000

    장고프로젝트-관리자 페이지 생성 및 기본 작성

--- main/models.py
+++ main/models.py
@@ -6,9 +6,15 @@ class Developer(models.Model):
     name=models.CharField(max_length=50)
     count=models.IntegerField(default=0)
     
+    def __str__(self):
+        return self.name
+    
 class Question(models.Model):
     number=models.IntegerField(unique=True)
     content=models.CharField(max_length=100)
+    
+    def __str__(self):
+        return f'{self.number}. {self.content}'
     
 class Choice(models.Model):
     content=models.CharField(max_length=100)
@@ -17,3 +23,6 @@ class Choice(models.Model):
     developer=models.ForeignKey(to='main.Developer',
                                on_delete=models.CASCADE,
                                null=True)
+    
+    def __str__(self):
+        return self.content
